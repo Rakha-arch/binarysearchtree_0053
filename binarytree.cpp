9357ed410e40ad3211387105200846e5ea39267d
Rakha-arch <miftahuzahran@gmail.com>
2025-06-26 11:07:39 +0700

    first commit

---
+++ binarytree.cpp
@@ -0,0 +1,19 @@
+// BinarySearchTree_4NIMBelakang
+// 15 Commit
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node
+    Node()
+    {
+        // isi constructor belum terlihat di gambar
+    }
+};
